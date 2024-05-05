5eff141a7e631973ad4a3e390836ef4f3c882d4f
Sagarika Jaiswal <sagarikajaiswal97@gmail.com>
2024-05-05 13:37:58 +0530

    feat: added bull board ui

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express, { Express } from "express";
 
+import serverAdapter from "./config/bullBoardUIConfig";
 import serverConfig from "./config/serverConfig";
 import { addJobs } from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
 import sampleWorker from "./workers/sampleWorker";
+
 const app: Express = express();
 
 app.use("/api", apiRouter);
+
+app.use("/queues/ui", serverAdapter.getRouter());
+
 app.listen(serverConfig.PORT, () => {
   console.log(`Server listening on port: ${serverConfig.PORT}`);
   sampleWorker("Sample Queue");
